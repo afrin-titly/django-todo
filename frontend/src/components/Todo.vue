82ca59f712ecd27169798ae84371dcefb227c65b
lm-afrin <titlee42@gmail.com>
2021-11-21 19:38:14 +0900

    show data from api call

---
+++ frontend/src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1> My TODO List</h1>
+  <ul v-if="list">
+    <li v-for="(l, index) in list" :key="index">
+      <h2> {{l.title}}</h2>
+      <p> {{l.description}} </p>
+      </li>
+  </ul>
+</template>
+
+<script>
+import axios from '../plugins/axios_plugin'
+export default {
+  data() {
+    return {
+      list: [],
+    }
+  },
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/')
+    .then(response => {
+      this.list = response.data
+    })
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
